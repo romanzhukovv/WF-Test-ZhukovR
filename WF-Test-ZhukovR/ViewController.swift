e0a50261f6cea7d60e217c0080eee81a84ddb258
romanzhukovv <zhukov.roman89@gmail.com>
2022-06-05 00:39:45 +0300

    Initial Commit

---
+++ WF-Test-ZhukovR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WF-Test-ZhukovR
+//
+//  Created by Roman Zhukov on 05.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
